aed9fc72a855b6c17c6abcc07289e9af4373ff7e
jaskaran2016 <80736653+jaskaran2016@users.noreply.github.com>
2021-06-06 15:30:31 +0800

    Add More Rooms

--- src/main/java/ass3/mygame2/RoomCreation.java
+++ src/main/java/ass3/mygame2/RoomCreation.java
@@ -1,8 +1,7 @@
+
 package ass3.mygame2;
 
-/**
- * 
- */
+
 import java.util.ArrayList;
 
 public class RoomCreation {
@@ -16,35 +15,45 @@ public class RoomCreation {
         createRooms();
     }
     
-    
     private void createRooms() {
 
-        Room castle, kitchen, frontGate, room1;
+        Room castle, kitchen, frontGate, Bedroom, Theatre;
 
         castle = new Room("castle", "You are at the castle", false);
         kitchen = new Room("kitchen", "The kitchen door has a shape of a heart", false);
         frontGate = new Room("frontGate", "There is a giant ogre", true);
-        room1 = new Room("room", "This is just for checking", true);
+        Bedroom = new Room("Bedroom", "This room has a bedroom", true);
+        Theatre = new Room("Theatre","There is a big projector", true);
         
 
         castle.setExit("east", kitchen);
         castle.setExit("south", frontGate);
         frontGate.setExit("north", castle);
-        frontGate.setExit("south", room1);
-        room1.setExit("north", frontGate);
+        frontGate.setExit("south", Bedroom);
+        Bedroom.setExit("north", frontGate);
+        Theatre.setExit("south", Bedroom);
+        
 
         castle.addItemInRoom(itemCreation.getItem("excaliburSword"));
         castle.addItemInRoom(itemCreation.getItem("key"));
         kitchen.addItemInRoom(itemCreation.getItem("frontGateKey"));
-        room1.addItemInRoom(itemCreation.getItem("gun"));
+        Bedroom.addItemInRoom(itemCreation.getItem("BedroomKey"));
+        Theatre.addItemInRoom(itemCreation.getItem("RemoteControl"));
 
         allRoomInGame.add(castle);
         allRoomInGame.add(frontGate);
         allRoomInGame.add(kitchen);
-        allRoomInGame.add(room1);
+        allRoomInGame.add(Bedroom);
+        allRoomInGame.add(Theatre);
 
     }
 
+  /**
+   * 
+   * @param stringRoom Contains the room name
+   * @return the room name
+   * 
+   */
     public Room getRoom(String stringRoom) {
         Room roomToReturn = null;
         for (Room room : allRoomInGame) {
